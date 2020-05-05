b6b07a0df35970c2571c09d2761712b4fbca02cf
Omar Mirza <omar.creative.dev@gmail.com>
2020-05-05 09:28:08 +0100

    turn off view encapsulation for css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   public title = 'Apple MacBook Pro 15.4" 1TB 16GB RAM 2.9Ghz i7 Laptop with Touchbar (Late 2016)';
