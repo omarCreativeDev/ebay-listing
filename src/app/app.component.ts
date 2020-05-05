8ec5d100ccc3a1817cf52928153e4962f14d4239
Omar Mirza <omar.creative.dev@gmail.com>
2020-05-05 14:45:34 +0100

    final tweaks to template to comply with ebay rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   public title = 'Apple MacBook Pro 15.4" 1TB 16GB RAM 2.9Ghz i7 Laptop with Touchbar (Late 2016)';
