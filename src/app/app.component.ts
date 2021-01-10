5b567cd98dcfdc79282a357ebcbb4ec33ff37177
Omar Mirza <omar.creative.dev@gmail.com>
2021-01-10 10:14:04 +0000

    prepped iphone ebay listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  public title = 'Brand NEW Sony PS5 Playstation 5 (PS5) Blu-Ray Disc Edition Console';
+  public title = "factory unlocked Apple iPhone Xs Max 256Gb in Space Grey";
 }
