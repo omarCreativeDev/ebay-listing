ad9f90c13c990e8c694b78e48a047908b757241c
Omar Mirza <omar.creative.dev@gmail.com>
2022-10-06 23:56:57 +0100

    fix unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders creative developments.net in footer', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Creative Developments.net');
   expect(linkElement).toBeInTheDocument();
 });
