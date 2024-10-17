dfbfd30465b0e3cfc465972ab617e7c9fc3dcdb8
Omar Mirza <omar.creative.dev@gmail.com>
2024-10-17 08:08:12 +0100

    tweaked keywords

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const TITLE =
   '21x Sleeved Pokémon TCG Cards - Crown Zenith Bulk/Job Lot/Bundle Sword and Shield';
-export const TITLE_AND_KEYWORDS = `${TITLE}, UK seller, no reserve`;
+export const KEYWORDS = `UK seller, no reserve`;
 export const SPECS_URL = 'https://www.pokedata.io/set/Crown+Zenith';
 export const FEATURES = [];
