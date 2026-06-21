73491fc5291b2c21a97aabdf462add0c43f4a0ec
Omar Mirza <omar.creative.dev@gmail.com>
2026-06-21 06:22:50 +0100

    added btn that will copy and paste style tags plus html

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,91 @@
+import { useRef } from 'react';
 import styles from './App.module.scss';
 import { EBAY_ID, FEATURES, TITLE } from './constants';
 import classNames from 'classnames';
 import { Content } from './Content/Content';
+import { CopyStyleAndHtmlBtn } from './CopyStyleAndHtmlBtn/CopyStyleAndHtmlBtn';
 import { Logo } from './Logo/Logo';
 
 function App() {
   const { ebayListing, heading, h1, wrapper, mainSection, spacer, content, link } = styles;
+  const markupRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div className={ebayListing}>
-      {/*<Logo />*/}
-      <h1 className={classNames(h1, heading)}>{TITLE}</h1>
-      <div className={wrapper}>
-        <div className={mainSection}>
-          <h2 className={heading}>Description</h2>
-          <div className={content}>
-            <Content />
-
-            {FEATURES.length > 0 ? (
-              <>
-                <p>
-                  <strong>Features:</strong>
-                </p>
-
-                <ul>
-                  {FEATURES.map((feature) => (
-                    <li key={feature}>{feature}</li>
-                  ))}
-                </ul>
-              </>
-            ) : null}
-
-            <p>
-              Please also checkout&nbsp;
-              <strong>
-                <a
-                  href={`https://www.ebay.co.uk/sch/${EBAY_ID}/m.html`}
-                  target="_blank"
-                  className={link}
-                  rel="noreferrer"
-                >
-                  our other listings
-                </a>
-              </strong>
-              .
-            </p>
-          </div>
-        </div>
-
-        <div>
-          <div className={spacer}>
-            <h3 className={heading}>Packaging</h3>
+    <>
+      <div className={ebayListing} ref={markupRef}>
+        {/*<Logo />*/}
+        <h1 className={classNames(h1, heading)}>{TITLE}</h1>
+        <div className={wrapper}>
+          <div className={mainSection}>
+            <h2 className={heading}>Description</h2>
             <div className={content}>
-              <ul>
-                <li>
-                  Boxes/tins or slabs are bubble wrapped and then packed securely for extra peace of
-                  mind.
-                </li>
-                <li>
-                  Cards will be sent in a penny sleeve plus a card saver and then dispatched with a
-                  hard card board backed envelope.
-                </li>
-              </ul>
+              <Content />
+
+              {FEATURES.length > 0 ? (
+                <>
+                  <p>
+                    <strong>Features:</strong>
+                  </p>
+
+                  <ul>
+                    {FEATURES.map((feature) => (
+                      <li key={feature}>{feature}</li>
+                    ))}
+                  </ul>
+                </>
+              ) : null}
+
+              <p>
+                Please also checkout&nbsp;
+                <strong>
+                  <a
+                    href={`https://www.ebay.co.uk/sch/${EBAY_ID}/m.html`}
+                    target="_blank"
+                    className={link}
+                    rel="noreferrer"
+                  >
+                    our other listings
+                  </a>
+                </strong>
+                .
+              </p>
             </div>
           </div>
 
           <div>
-            <h3 className={heading}>Shipping</h3>
-            <div className={content}>
-              <ul>
-                <li>
-                  Combined postage available for multiple purchases. Please get in contact for more
-                  info.
-                </li>
-                <li>International shipping and collection not available.</li>
-              </ul>
+            <div className={spacer}>
+              <h3 className={heading}>Packaging</h3>
+              <div className={content}>
+                <ul>
+                  <li>
+                    Boxes/tins or slabs are bubble wrapped and then packed securely for extra peace
+                    of mind.
+                  </li>
+                  <li>
+                    Cards will be sent in a penny sleeve plus a card saver and then dispatched with
+                    a hard card board backed envelope.
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div>
+              <h3 className={heading}>Shipping</h3>
+              <div className={content}>
+                <ul>
+                  <li>
+                    Combined postage available for multiple purchases. Please get in contact for
+                    more info.
+                  </li>
+                  <li>International shipping and collection not available.</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <CopyStyleAndHtmlBtn targetRef={markupRef} />
+    </>
   );
 }
 
---
+++ src/CopyStyleAndHtmlBtn/CopyStyleAndHtmlBtn.tsx
@@ -0,0 +1,35 @@
+import { RefObject, useState } from 'react';
+import classNames from 'classnames';
+import styles from './CopyStyleAndHtmlBtn.module.scss';
+
+interface CopyButtonProps {
+  targetRef: RefObject<HTMLDivElement | null>;
+}
+
+export const CopyStyleAndHtmlBtn = ({ targetRef }: CopyButtonProps) => {
+  const { btn, copySuccess } = styles;
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      const divHtml = targetRef.current ? targetRef.current.innerHTML : '';
+      const styleTags = document.head.querySelectorAll('style');
+      const styleHtml = Array.from(styleTags)
+        .map((tag) => tag.outerHTML)
+        .join('\n');
+      const combinedContent = `${styleHtml}\n${divHtml}`;
+      await navigator.clipboard.writeText(combinedContent);
+
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch (err) {
+      console.error('Failed to copy text: ', err);
+    }
+  };
+
+  return (
+    <button onClick={handleCopy} className={classNames(btn, copied ? copySuccess : '')}>
+      {copied ? 'Copied to Clipboard! ✅' : 'Copy Style & HTML'}
+    </button>
+  );
+};
